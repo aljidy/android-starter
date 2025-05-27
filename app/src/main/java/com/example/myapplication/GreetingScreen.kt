cfc8f8da2fcc6aeda3915782d61865a0b74ce991
Adam <adam.aljidy@gmail.com>
2025-05-27 23:33:21 +0100

    Add DetailScreen, add navigation between mainScreen and detailScreen

--- app/src/main/java/com/example/myapplication/GreetingScreen.kt
+++ app/src/main/java/com/example/myapplication/GreetingScreen.kt
@@ -1,21 +1,37 @@
 package com.example.myapplication
 
+ import android.widget.Toast
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 @Composable
-fun GreetingScreen() {
+fun GreetingScreen(onNextButtonTap: () -> Unit) {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Text(
-            text = "Hello ${"Android"}!",
-            modifier = Modifier.padding(innerPadding),
-        )
+        Column(
+            modifier =
+                Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize(),
+        ) {
+            Text(text = "Hello Android!")
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Button(onClick = onNextButtonTap, modifier = Modifier.align(Alignment.End)) {
+                Text("Next Screen")
+            }
+        }
     }
 }
 
@@ -23,6 +39,11 @@ fun GreetingScreen() {
 @Composable
 private fun GreetingScreenPreview() {
     MyApplicationTheme {
-        GreetingScreen()
+        val context = LocalContext.current
+
+        GreetingScreen {
+            Toast.makeText(context, "Button tapped", Toast.LENGTH_LONG).show()
+            // TODO make this something that shows in interactive mode
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,17 +4,44 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Greeting
+
+@Serializable
+data class Detail(val name: String)
+
+const val GREETING_NAME: String = "Friend"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
+
             MyApplicationTheme {
-                GreetingScreen()
+                NavHost(navController = navController, startDestination = Greeting) {
+                    composable<Greeting> {
+                        GreetingScreen {
+                            navController.navigate(route = Detail(name = GREETING_NAME))
+                        }
+                    }
+
+                    composable<Detail> { backStackEntry ->
+                        val detail: Detail = backStackEntry.toRoute()
+                        DetailScreen(
+                            name = detail.name,
+                            onNavigateBackTapped = { navController.popBackStack() },
+                        )
+                    }
+                }
             }
         }
     }
